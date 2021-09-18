b6faca195e1dc683467f1abb716b88fb1308b673
bbhxwl <470138890@qq.com>
2021-09-18 09:42:07 +0800

    添加项目文件。

---
+++ LocalStaticSite/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocalStaticSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        IWebHostEnvironment env;
+        public HomeController(IWebHostEnvironment _env)
+        {
+
+            env = _env;
+        }
+        public IActionResult Index()
+        {
+            var rootPath = env.WebRootPath;
+            string path = Path.Combine(rootPath, "Download/");
+            var list = System.IO.Directory.GetFiles(path).ToList();
+            for (int i = 0; i < list.Count; i++)
+            {
+               list[i]= Path.GetFileName(list[i]);
+            }
+
+            return View(list);
+        }
+        public IActionResult Download(string filename) {
+             
+            return Redirect("/Download/"+ filename);
+        }
+    }
+}
